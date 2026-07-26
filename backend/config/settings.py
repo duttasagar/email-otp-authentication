108bb234778ef31b7894bee1ee67e5f8cc76b552
Sagar Dutta <duttasagar39@gmail.com>
2026-07-26 16:21:46 +0530

    Fix Brevo SMTP configuration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -186,14 +186,16 @@ AUTH_USER_MODEL = "accounts.User"
 
 
 
+
+
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
-EMAIL_HOST_USER = os.getenv("BREVO_EMAIL")
-EMAIL_HOST_PASSWORD = os.getenv("BREVO_SMTP_KEY")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
-
 
 
 
